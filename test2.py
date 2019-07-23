cdf8aae10a2a737ba01e4592654ab8adeffc39de
Payalkabra <33509486+Payalkabra@users.noreply.github.com>
2019-07-23 12:08:48 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -8,4 +8,4 @@ def main():
     return "welcome to flask"
 
 if __name__ == "__main__":
-    app.run(debug = True , host="127.0.0.1" , port =8000)    
+    app.run(debug = True , host="0.0.0.0" , port =8000)    
