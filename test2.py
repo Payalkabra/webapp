e03f2c8248a6d80c8f0f483185e14a1f90d4af09
Payalkabra <33509486+Payalkabra@users.noreply.github.com>
2019-07-23 10:56:48 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 def main():
 
-    return "welcome"
+    return "welcome to flask"
 
 if __name__ == "__main__":
     app.run(debug = True , host="127.0.0.1" , port =8000)    
